01d9db1592d650023f84d3c8c9d769a3b7891a43
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-11-18 11:45:52 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const pic = require('random-pic.js') // npm install random-pic.js
+
+// do every thing with response
+
+// pic.animal().then((response) => { console.log(response) })
+// pic.anime().then((response) => { response })
+// pic.boy().then((response) => { response })
+// pic.girl().then((response) => { response })
+// pic.couple().then((response) => { response })
+// pic.eboy().then((response) => { response })
+// pic.egirl().then((response) => { console.log(response) })
+pic.landscape().then((response) => { console.log(response) })
+// pic.random().then((response) => { console.log(response) })
